6ba6fe728746f18fc1fa2b0e900dcadb7e3b9421
mitko17 <mitkodim739@gmail.com>
2022-04-01 11:17:27 +0300

    Last changes

--- TestApplication/TestApplication/Example.cpp
+++ TestApplication/TestApplication/Example.cpp
@@ -12,25 +12,30 @@ int sumMore(int a, int b,int c)
 
 int multiply(int a, int b)
 {
-	return a*b;
+	return a * b;
 }
 
 int divideByTwo(int a)
 {
-	return a/2;
+	return a / 2;
 }
 
 int substract(int a, int b)
 {
 	return a - b;
 }
+
 float squareRoot(int a)
 {
-	//int c;
 	return sqrt(a);
 }
-int divide(int a, int b)
+
+float divide(int a, int b)
 {
-	if (b!=0) return a / b;
-	return a;
+	if (b != 0) return a / static_cast<float> (b);
+}
+
+int modulus(int a, int b)
+{
+	return a % b;
 }
--- TestApplication/TestApplication/TestApplication.cpp
+++ TestApplication/TestApplication/TestApplication.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
-    cout << "a=";
-    cin >> a;
-    cout << "b=";
-    cin >> b;
-    cout << "c=";
-    cin >> c;
-    cout << "The sum of "<<a<<" and "<<b<<" is "<<sum(a,b)<<endl;
-    cout << "The sum of 3 numbers is: "  << sumMore(a, b,c) << endl;
-    cout << "The multiplication of " << a << " and " << b << " is " << multiply(a, b) << endl;
-    cout << "Divided by two: "<<divideByTwo(a)<<endl;   
-    cout<< "The substraction of " << a << " and " << b << " is " << substract(a, b) << endl;
+	int a, b, c;
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	cout << "c = ";
+	cin >> c;
+	cout << "The sum of " << a << " and " << b << " is " << sum(a, b) << endl;
+	cout << "The sum of 3 numbers is: " << sumMore(a, b, c) << endl;
+	cout << "The multiplication of " << a << " and " << b << " is " << multiply(a, b) << endl;
+	cout << "Divided by two: " << divideByTwo(a) << endl;
+	cout << "The substraction of " << a << " and " << b << " is " << substract(a, b) << endl;
 
-    cout << "I have branch" << endl;
+	cout << "I have branch" << endl;
 
-    cout<< "The squareRoot of  sum " << a << " and " << b << " is " << squareRoot(a, b) << endl;
-    cout << "The number " << a << " divided by the number " << b << " is: " << divide(a, b) << endl;
+	cout << "The squareRoot of " << a << " is " << squareRoot(a) << endl;
+	cout << "The number " << a << " divided by the number " << b << " is: " << divide(a, b) << endl;
+	cout << "Modulus of " << a << " and " << b << " is: " << modulus(a, b) << endl;
 }
-
